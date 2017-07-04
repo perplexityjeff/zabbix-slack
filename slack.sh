47c03a1b5c108a6c1165c861d0ef27a394928df2
Jeffrey Jansma <me@perplexity.nl>
2017-07-04 12:49:46 +0200

    Update slack.sh

--- slack.sh
+++ slack.sh
@@ -24,15 +24,23 @@ chart_basedir="/tmp/slack_charts"
 chart_cookie="/tmp/zcookies.txt"
 
 # set params
-host="`echo \"${params}\" | grep 'HOST: ' | awk -F'HOST: ' '{print $2}' | sed -e 's///g'`"
-trigger_name="`echo \"${params}\" | grep 'TRIGGER_NAME: ' | awk -F'TRIGGER_NAME: ' '{print $2}' | sed -e 's///g'`"
-trigger_status="`echo \"${params}\" | grep 'TRIGGER_STATUS: ' | awk -F'TRIGGER_STATUS: ' '{print $2}' | sed -e 's///g'`"
-trigger_severity="`echo \"${params}\" | grep 'TRIGGER_SEVERITY: ' | awk -F'TRIGGER_SEVERITY: ' '{print $2}' | sed -e 's///g'`"
-trigger_url="`echo \"${params}\" | grep 'TRIGGER_URL: ' | awk -F'TRIGGER_URL: ' '{print $2}' | sed -e 's///g'`"
-datetime="`echo \"${params}\" | grep 'DATETIME: ' | awk -F'DATETIME: ' '{print $2}' | sed -e 's///g'`"
-item_value="`echo \"${params}\" | grep 'ITEM_VALUE: ' | awk -F'ITEM_VALUE: ' '{print $2}' | sed -e 's///g'`"
-event_id="`echo \"${params}\" | grep 'EVENT_ID: ' | awk -F'EVENT_ID: ' '{print $2}' | sed -e 's///g'`"
-item_id="`echo \"${params}\" | grep 'ITEM_ID: ' | awk -F'ITEM_ID: ' '{print $2}' | sed -e 's///g'`"
+host="`echo \"${params}\" | grep 'HOST: ' | awk -F 'HOST: ' '{print $2}' | tr -d '\r'`"
+
+trigger_name="`echo \"${params}\" | grep 'TRIGGER_NAME: ' | awk -F 'TRIGGER_NAME: ' '{print $2}' | tr -d '\r'`"
+
+trigger_status="`echo \"${params}\" | grep 'TRIGGER_STATUS: ' | awk -F 'TRIGGER_STATUS: ' '{print $2}' | tr -d '\r'`"
+
+trigger_severity="`echo \"${params}\" | grep 'TRIGGER_SEVERITY: ' | awk -F 'TRIGGER_SEVERITY: ' '{print $2}' | tr -d '\r'`"
+
+trigger_url="`echo \"${params}\" | grep 'TRIGGER_URL: ' | awk -F 'TRIGGER_URL: ' '{print $2}' | tr -d '\r'`"
+
+datetime="`echo \"${params}\" | grep 'DATETIME: ' | awk -F 'DATETIME: ' '{print $2}' | tr -d '\r'`"
+
+item_value="`echo \"${params}\" | grep 'ITEM_VALUE: ' | awk -F 'ITEM_VALUE: ' '{print $2}' | tr -d '\r'`"
+
+event_id="`echo \"${params}\" | grep 'EVENT_ID: ' | awk -F 'EVENT_ID: ' '{print $2}' | tr -d '\r'`"
+
+item_id="`echo \"${params}\" | grep 'ITEM_ID: ' | awk -F 'ITEM_ID: ' '{print $2}' | tr -d '\r'`"
 
 # get charts
 if [ "${item_id}" != "" ]; then
@@ -45,12 +53,12 @@ if [ "${item_id}" != "" ]; then
 
     # if triger url is empty then we link to the graph with the item_id
     if [ "${trigger_url}" == "" ]; then
-        trigger_url="${zabbix_baseurl}/history.php?action=showgraph&itemids=${item_id}"
+        trigger_url="${zabbix_baseurl}/history.php?action=showgraph&itemids[]=${item_id}"
     fi
 fi
 
 # set color
-if [ "${trigger_status}" == 'OK' ]; then
+if [ "${trigger_status}" == "OK" ]; then
   case "${trigger_severity}" in
     'Information')
       color="#439FE0"
@@ -59,7 +67,7 @@ if [ "${trigger_status}" == 'OK' ]; then
       color="good"
       ;;
   esac
-elif [ "${trigger_status}" == 'PROBLEM' ]; then
+elif [ "${trigger_status}" == "PROBLEM" ]; then
   case "${trigger_severity}" in
     'Information')
       color="#439FE0"
@@ -115,4 +123,3 @@ payload="payload={
 
 # send to slack
 ${cmd_curl} -m ${timeout} --data-urlencode "${payload}" "${slack_url}"
-
